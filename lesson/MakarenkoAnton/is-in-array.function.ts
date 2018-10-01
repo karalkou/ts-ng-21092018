e3c6d074479eb9183e15618ea8a0fb9e192cdf41
Anton Makarenko <anton@makarenko.com>
2018-10-01 17:16:10 -0500

    lesson 1 - getUnique()

--- lesson/MakarenkoAnton/is-in-array.function.ts
+++ lesson/MakarenkoAnton/is-in-array.function.ts
@@ -1,8 +1,8 @@
-type PRIMITIVE = (string|number|boolean|symbol|object);
+type ALMOSTANY = (string|number|boolean|symbol|object);
 
 // QUESTION: is it OK to mix type declaration (side-effect) and function declaration in one file?
 
-function isInArray(haystack: PRIMITIVE[], first: PRIMITIVE, ...needles: PRIMITIVE[]): boolean {
+function isInArray(haystack: ALMOSTANY[], first: ALMOSTANY, ...needles: ALMOSTANY[]): boolean {
     const all = [].concat([first], needles);
     for (const el of all) {
         if (haystack.indexOf(el) === -1) {
--- lesson/MakarenkoAnton/main.ts
+++ lesson/MakarenkoAnton/main.ts
@@ -1,5 +1,8 @@
 import './is-in-array.function'; // QUESTION: is this kind of import good enough or we need more elaborate structure?
 import './summator.function';
+import './get-unique.function';
+
+// QUESTION: what is the proper way to execute this, since console.log() is not recommended?
 
 // isInArray([]); // not gonna work, missing mandatory 2nd parameter
 isInArray([], 1);
@@ -10,3 +13,11 @@ isInArray(['foo', 'bar'], 'foo');
 // summator(1, 'foo', 'bar'); // not allowed, must have all either number or all string
 summator(1, 2, 3);
 summator('foo', 'bar', 'baz');
+
+// getUnique(); // not allowed, must have at least 1
+getUnique([1, 2, 3], [1, 3, 5], [8, 13]); // [1, 2, 3, 5, 8, 13]
+const a = {};
+const b = {};
+const c = {};
+getUnique([a, b], [b, c]); // [a, b, c]
+
