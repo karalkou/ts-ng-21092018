9323c0541c3851a79cfad6ca9d1cb5a816308455
sfs-inep <igornepipenko@gmail.com>
2018-10-09 22:05:27 +0300

    finish rxjs

---
+++ lesson/rxjs-2-class-work.ts
@@ -0,0 +1,69 @@
+import { interval, of } from 'rxjs';
+import { mergeMap, take, tap } from 'rxjs/operators';
+
+//const sequence$: Observable<number> = from([1, 2, 3, 4, 5]);
+
+// class DoubleSubscriber extends Subscriber<number> {
+//     public _next(value: number) {
+//         this.destination.next && this.destination.next(value * 2);
+//     }
+// }
+//
+// const double = (source: Observable<number>) => {
+//     return source.lift({
+//         call(subscribe: Subscriber<number>) {
+//             source.subscribe(new DoubleSubscriber(subscribe));
+//         }
+//     });
+// };
+//
+// sequence$.pipe(double)
+//     .subscribe((value: number) => {
+//         console.log(value);
+//     });
+
+
+// class MultiplySubscriber extends Subscriber<number> {
+//
+//     private readonly num: number;
+//
+//     public constructor(subscribe: Subscriber<number>, num: number) {
+//         super(subscribe);
+//         this.num = num;
+//     }
+//
+//     public _next(value: number) {
+//         this.destination.next && this.destination.next(value * this.num);
+//     }
+// }
+//
+// const multi = (num: number) => (source: Observable<number>) => {
+//     return source.lift({
+//         call(subscribe: Subscriber<number>) {
+//             source.subscribe(new MultiplySubscriber(subscribe, num));
+//         }
+//     });
+// };
+//
+// sequence$.pipe(multi(3))
+//     .subscribe((value: number) => {
+//         console.log(value);
+//     });
+// customMap((x)=>x*4)
+
+const sequence$ = interval(1000)
+    .pipe(take(4));
+
+
+const hightOrderObservable = sequence$.pipe(
+    tap((x) => {
+        console.log(`X=> ${x}`);
+        return x * 3;
+    }),
+    mergeMap((_x: number) => {
+        console.log(`_x=> ${_x}`);
+        return of(1, 2);
+    }),
+);
+
+hightOrderObservable.subscribe((x) => console.log(x));
---
+++ src/index.ts
@@ -0,0 +1,97 @@
+// import { fromEvent } from 'rxjs';
+// import { switchMap } from 'rxjs/operators';
+//
+// const clickSequence$ = fromEvent(document, 'click');
+//
+// function request() {
+//     return fetch('https://jsonplaceholder.typicode.com/users/1')
+//         .then((res) => res.json());
+// }
+//
+// const requestSequence$ = clickSequence$.pipe(
+//     switchMap((_click) => request()
+//     )
+// );
+//
+// requestSequence$.subscribe((email) => {
+//     console.log(email);
+// });
+
+// Observable + Observer = Subject
+
+//  controlSeauence$$ = new ReplaySubject();
+
+// controlSeauence$$.subscribe((value => {
+//     console.log(`A => ${value}`);
+// }));
+
+// controlSeauence$$.next(10);
+// controlSeauence$$.next(11);
+// controlSeauence$$.next(15);
+//
+// setTimeout(() => {
+//     controlSeauence$$.subscribe((value => {
+//         console.log(`C => ${value}`);
+//     }));
+//     // controlSeauence$$.complete();
+// }, 2000);
+
+// setTimeout(() => {
+//     controlSeauence$$.subscribe((value => {
+//         console.log(`B => ${value}`);
+//     }));
+// }, 5000);
+// const controlSequence$$ = new Subject();
+// const connectableSequence$: ConnectableObservable<number> = interval(1000)
+//     .pipe(
+//         // take(5),
+//         tap((x) => {
+//             console.log(x);
+//         }),
+//         // multicast(controlSequence$$),
+//         publish(),
+//     ) as ConnectableObservable<number>;
+// const autoConnectableObserver  = connectableSequence$.refCount();
+//
+// const subA: Subscription = autoConnectableObserver.subscribe((value => {
+//     console.log(`A => ${value}`);
+// }));
+// let subB: Subscription;
+// setTimeout(() => {
+//     subA.unsubscribe();
+//     subB = autoConnectableObserver.subscribe((value => {
+//         console.log(`B => ${value}`);
+//     }));
+//     // controlSeauence$$.complete();
+// }, 2000);
+//
+// setTimeout(() => {
+//     subB.unsubscribe();
+//     // controlSeauence$$.complete();
+// }, 4000);
+
+//TODO windowTime, multicast
+
+import { from } from 'rxjs';
+import { map, observeOn } from 'rxjs/operators';
+import { async } from 'rxjs/internal/scheduler/async';
+import { asap } from 'rxjs/internal/scheduler/asap';
+
+let arr = [];
+for (let i = 0; i < 5000; i++) {
+    arr.push(i);
+}
+const sequence$ = from(arr);
+
+console.log('start');
+console.time('example');
+sequence$.pipe(
+    observeOn(asap),
+    map(x => x * 2 / 4)
+)
+    .subscribe((_x) => {
+
+    }, () => {}, () => {
+        console.timeEnd('example');
+    });
+console.log('end');
